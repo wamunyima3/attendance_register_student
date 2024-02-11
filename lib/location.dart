4599bc18eac2fa695998de686f43ae523e35c0e0
wamunyima3 <wamunyimamukelabai3@gmail.com>
2024-02-11 22:38:46 +0200

    Added launcher icon

--- lib/location.dart
+++ lib/location.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future<void> checkAndRequestPermissions() async {
+  // Check if permission is granted
+  PermissionStatus locationPermissionStatus = await Permission.location.status;
+
+  if (locationPermissionStatus != PermissionStatus.granted) {
+    // Request permission
+    await Permission.location.request();
+  }
+}
 
 class LocationService {
   Future<Position> getCurrentLocation() async {
@@ -23,6 +34,10 @@ class LocationService {
 Future<Map<String, dynamic>> getLocation(BuildContext context) async {
   LocationService locationService = LocationService();
   try {
+    // Check and request location permission
+    await checkAndRequestPermissions();
+
+    // Get current location
     Position position = await locationService.getCurrentLocation();
     double latitude = position.latitude;
     double longitude = position.longitude;
